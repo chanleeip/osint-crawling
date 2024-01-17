e123c1d02e56e20f7504680a998c2469442b1d22
chanleeip <chanleeip@gmail.com>
2024-01-17 21:47:27 +0530

    added subprocess

--- app.py
+++ app.py
@@ -1,15 +1,34 @@
 from flask import Flask, render_template,request
-
+import subprocess
 app = Flask(__name__)
 
 @app.route('/',methods=['POST','GET'])
 def hello_world():
     if request.method == 'POST':
         domain = request.form['domain']
-        print(domain)
-        return render_template('index.html',domain=domain)
+        options=request.form.getlist('options')
+        scripts=request.form.getlist('scripts')
+        print(scripts)
+        if scripts[0]=="hydrarecon":
+            result=['python3','crawlers/HydraRecon/hydrarecon.py']
+            result.extend(options)
+            result.extend(["-d",domain])
+            ans=subprocess.Popen(result,stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            ans.wait()
+            output=ans.stdout.read().decode()
+            return render_template('result.html',result=output)
+        if scripts[0]=="finalrecon":
+            result=['python3','crawlers/FinalRecon/finalrecon.py']
+            result.extend(options)
+            result.extend([domain])
+            print(result)
+            ans=subprocess.Popen(result,stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            ans.wait()
+            output=ans.stdout.read().decode()
+            return render_template('result.html',result=output)
     else:
-        return render_template('index.html', domain=None)
+        return render_template('index.html')
+
 
 
 if __name__ == '__main__':
